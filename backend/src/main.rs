666dd6b2a3d63aa39d9ed12a26858db7f361113b
Sebastien Klasa <klasa.sebastien@gmail.com>
2023-05-08 21:32:24 +0000

    feat(back): add /api/events route that returns hard coded events

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,7 +5,39 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/api/events")]
+fn events() -> &'static str {
+    "[{\"name\": \"event_1\"}, {\"name\": \"event_2\"}, {\"name\": \"event_3\"}]"
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, events])
+}
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::http::Status;
+    use rocket::local::blocking::Client;
+
+    #[test]
+    fn list_all_events() {
+        // given 3 existing events
+        // TODO: populate events
+
+        // when a user requests /api/events
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get(uri!("/api/events")).dispatch();
+
+        // then the server responds with a list containing all 3 events
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(
+            response.into_string(),
+            Some(
+                "[{\"name\": \"event_1\"}, {\"name\": \"event_2\"}, {\"name\": \"event_3\"}]"
+                    .into()
+            )
+        ); // TODO: deserialize JSON
+    }
 }
