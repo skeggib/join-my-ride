ef0855cfa2672b8e6a73ca5919254b93bafc1c2b
Sebastien Klasa <klasa.sebastien@gmail.com>
2023-06-25 09:46:47 +0000

    feat(script): watch and run tests in each folder separately

--- dev_tmux.sh
+++ dev_tmux.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 tmux new-session \; \
-    send-keys 'cargo watch -x test' C-m \; \
+    rename-window serve \; \
+    send-keys 'cd backend/; cargo watch -x run' C-m \; \
     split-window -h \; \
-    send-keys 'cd backend/; cargo watch -C .. -x "run backend"' C-m \; \
-    split-window -v \; \
-    send-keys 'cd frontend/; cargo make watch' C-m
+    send-keys 'cd frontend/; cargo make watch' C-m \; \
+    new-window \; \
+    rename-window test \; \
+    send-keys 'cd backend/; cargo watch -x test' C-m \; \
+    split-window -h \; \
+    send-keys 'cd common/; cargo watch -x test' C-m
