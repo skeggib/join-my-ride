d2b8fd9a91a13503c0dcb19c7693c34043c620b0
Sebastien Klasa <klasa.sebastien@gmail.com>
2023-06-25 10:18:27 +0000

    feat(front): display event participants and add a join button

--- frontend/src/component/event.rs
+++ frontend/src/component/event.rs
@@ -1,5 +1,5 @@
-use crate::rest::get_json;
-use common::Event;
+use crate::rest::{get_json, put};
+use common::{Event, Id};
 use seed::{prelude::*, *};
 use std::future::Future;
 
@@ -19,6 +19,19 @@ pub fn request_event(id: Id, orders: &mut impl Orders<Msg>) {
     });
 }
 
+pub fn join_event(id: Id, orders: &mut impl Orders<Msg>) {
+    log!("join event {}", id);
+    orders.perform_cmd(async move {
+        match put(&format!("/api/join/{}", id)).await {
+            Ok(_) => match get_event(id).await {
+                Ok(event) => Msg::OnGetEventResponse(event),
+                Err(error) => Msg::Error(error),
+            },
+            Err(error) => Msg::Error(error),
+        }
+    });
+}
+
 // TODO: move to common code
 fn perform_cmd(orders: &mut impl Orders<Msg>, cmd: impl Future<Output = Msg> + 'static) {
     orders.perform_cmd(cmd);
@@ -28,15 +41,29 @@ fn perform_cmd(orders: &mut impl Orders<Msg>, cmd: impl Future<Output = Msg> + '
 pub enum Msg {
     OnGetEventResponse(Event),
     Error(String),
+    JoinEventClick,
 }
 
-pub fn update(msg: Msg) -> Result<Model, String> {
+pub fn update(msg: Msg, model: Model, orders: &mut impl Orders<Msg>) -> Result<Model, String> {
     match msg {
         Msg::OnGetEventResponse(event) => Ok(event),
         Msg::Error(error) => Err(error),
+        Msg::JoinEventClick => {
+            join_event(model.id, orders);
+            Ok(model)
+        }
     }
 }
 
 pub fn view(model: &Model) -> Node<Msg> {
-    div![&model.name]
+    let participants_divs: Vec<Node<Msg>> = model
+        .participants
+        .iter()
+        .map(|participant| div![participant.clone()])
+        .collect();
+    div![
+        div![&model.name],
+        div![participants_divs],
+        div![button!["join", ev(Ev::Click, |_| Msg::JoinEventClick)]],
+    ]
 }
