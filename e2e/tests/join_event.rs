de292138f62a371e6b482f8ba2c7dcd3ac3aecdf
Sebastien Klasa <klasa.sebastien@gmail.com>
2023-12-20 19:51:39 +0000

    wip: given a logged in user (cannot call wasm code in Url)

--- e2e/tests/join_event.rs
+++ e2e/tests/join_event.rs
@@ -2,67 +2,23 @@ use async_trait::async_trait;
 use backend::Backend;
 use common::api::BackendApi;
 use common::{Event, Id};
+use debug_cell::RefCell;
 use frontend::{
     app::{self, testable_update, Msg},
     orders::{MyOrders, OrdersStub},
 };
+use seed::app::subs::UrlChanged;
+use seed::virtual_dom::At;
 use seed::{
     virtual_dom::{Node, Tag},
     Url,
 };
-use std::{ rc::Rc};
-use debug_cell::RefCell;
+use std::borrow::BorrowMut;
+use std::rc::Rc;
+use std::str::FromStr;
 
-#[test]
-fn clicking_join_on_event_adds_user_to_participants() {
-    let messages: Rc<RefCell<Vec<Msg>>> = Rc::new(RefCell::new(vec![]));
-    let messages_clone = messages.clone();
-    let orders = Rc::new(RefCell::new(MyOrders::new(
-        frontend::orders::OrdersImplementation::<Msg, Msg>::Stub(OrdersStub::new(Rc::new(
-            RefCell::new(move |msg| messages_clone.borrow_mut().push(msg)),
-        ))),
-    )));
-    let backend = SyncBackend::new();
-
-    let app_ = Rc::new(RefCell::new(app::testable_init(
-        Url::new(),
-        &mut *orders.borrow_mut(),
-        Rc::new(backend),
-    )));
-
-    let cloned_app_ = app_.clone();
-    let cloned_orders = orders.clone();
-    let update = Rc::new(RefCell::new(move |msg| {
-        let mut app_ = cloned_app_.borrow_mut();
-        let mut orders = cloned_orders.borrow_mut();
-        testable_update(
-            msg,
-            &mut app_,
-            &mut *orders,
-        );
-        drop(app_);
-        drop(orders);
-    }));
-
-    for message in messages.borrow().iter() {
-        (update.borrow_mut())(message.clone());
-    }
-    messages.borrow_mut().clear();
-    match orders.borrow_mut().implementation {
-        frontend::orders::OrdersImplementation::Container(_) => todo!(),
-        frontend::orders::OrdersImplementation::Proxy(_) => todo!(),
-        frontend::orders::OrdersImplementation::Mock(_) => todo!(),
-        frontend::orders::OrdersImplementation::Stub(ref mut stub) => {
-            stub.update = update;
-        },
-        frontend::orders::OrdersImplementation::StubProxy(_) => todo!(),
-    };
-
-    // given a logged-in user
-
-    // and given the displayed page is en event
-    let view = app::view(&app_.borrow());
-    let event_url = find_node(&view, &|node| {
+fn get_login_url(root: &Node<Msg>) -> Option<&str> {
+    let login_a = find_node(&root, &|node| {
         node.is_el()
             && node.el().unwrap().tag == Tag::A
             && !node.el().unwrap().children.is_empty()
@@ -76,10 +32,95 @@ fn clicking_join_on_event_adds_user_to_participants() {
                 .text()
                 .unwrap()
                 .text
-                == "event_1"
-    });
-    assert!(event_url.is_some());
-    println!("{}", event_url.unwrap().to_string());
+                == "login"
+    })
+    .unwrap()
+    .el()
+    .unwrap();
+    let login_href = login_a.attrs.vals.get(&At::Href).unwrap();
+    if let seed::virtual_dom::values::AtValue::Some(login_url) = login_href {
+        Some(login_url)
+    } else {
+        None
+    }
+}
+
+#[test]
+fn clicking_join_on_event_adds_user_to_participants() {
+    let messages: Rc<RefCell<Vec<Msg>>> = Rc::new(RefCell::new(vec![]));
+    let messages_clone = messages.clone();
+    let orders = Rc::new(RefCell::new(MyOrders::new(
+        frontend::orders::OrdersImplementation::<Msg, Msg>::Stub(OrdersStub::new(Rc::new(
+            RefCell::new(move |msg| messages_clone.as_ref().borrow_mut().push(msg)),
+        ))),
+    )));
+    let backend = SyncBackend::new();
+
+    let app_ = Rc::new(RefCell::new(app::testable_init(
+        Url::new(),
+        &mut *orders.as_ref().borrow_mut(),
+        Rc::new(backend),
+    )));
+
+    let mut cloned_app_ = app_.clone();
+    let mut cloned_orders = orders.clone();
+    let update = Rc::new(RefCell::new(move |msg| {
+        let app_ = cloned_app_.borrow_mut();
+        let orders = cloned_orders.borrow_mut();
+        testable_update(
+            msg,
+            &mut app_.as_ref().borrow_mut(),
+            &mut *orders.as_ref().borrow_mut(),
+        );
+    }));
+
+    for message in messages.borrow().iter() {
+        (update.as_ref().borrow_mut())(message.clone());
+    }
+    messages.as_ref().borrow_mut().clear();
+    match orders.as_ref().borrow_mut().implementation {
+        frontend::orders::OrdersImplementation::Container(_) => todo!(),
+        frontend::orders::OrdersImplementation::Proxy(_) => todo!(),
+        frontend::orders::OrdersImplementation::Mock(_) => todo!(),
+        frontend::orders::OrdersImplementation::Stub(ref mut stub) => {
+            stub.update = update;
+        }
+        frontend::orders::OrdersImplementation::StubProxy(_) => todo!(),
+    };
+
+    // given a logged-in user
+    {
+        let view = app::view(&app_.borrow());
+        let login_url = get_login_url(&view).unwrap();
+        app::testable_update(
+            app::Msg::UrlChanged(UrlChanged(Url::from_str(login_url).unwrap())),
+            &mut app_.as_ref().borrow_mut(),
+            &mut *orders.as_ref().borrow_mut(),
+        );
+    }
+
+    // and given the displayed page is an event
+    {
+        let view = app::view(&app_.borrow());
+        let event_url = find_node(&view, &|node| {
+            node.is_el()
+                && node.el().unwrap().tag == Tag::A
+                && !node.el().unwrap().children.is_empty()
+                && node.el().unwrap().children.first().unwrap().is_text()
+                && node
+                    .el()
+                    .unwrap()
+                    .children
+                    .first()
+                    .unwrap()
+                    .text()
+                    .unwrap()
+                    .text
+                    == "event_1"
+        });
+        assert!(event_url.is_some());
+        println!("{}", event_url.unwrap().to_string());
+    }
     // app::testable_update(app::Msg::UrlChanged(UrlChanged(Url::from_str(format!("/event/{}", evend_id)))), model, orders)
 
     // when the user clicks on the join button of an event
